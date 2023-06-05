c6c99a2e9b04b0159b71c831d274c67dfe796b1f
rchowdhury99 <reizchowdhury@gmail.com>
2023-06-05 19:09:36 -0400

    added boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Intro from './components/Intro';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Intro />
+      <Experience />
+      <Projects />
     </div>
   );
 }
